dbe514a177c5f3d6a840514ab28e2cfb2b0b645b
aneetromana <aneetromana167@gmail.com>
2023-10-11 00:03:11 -0700

    new

--- screens/Cocktails.js
+++ screens/Cocktails.js
@@ -1,45 +1,76 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'; 
+import React, { useState } from 'react';
+import { ScrollView, Text, View, TouchableOpacity, Image } from 'react-native';
+import axios from 'axios';
 
-export default function About({ navigation }) {
+export default function Home() {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('margarita');
+
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+
+  const grabCocktailInfo = () => {
+    axios.get(url)
+      .then((response) => {
+        console.clear();
+        setData(response.data.drinks);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.textSize}>
-    cocktail recipes using API
-      </Text>
-      <StatusBar style="auto" />
+    <ScrollView style={styles.container}>
       <TouchableOpacity
-        style={styles.customButton}
-        onPress={() => navigation.goBack()}
+        onPress={() => grabCocktailInfo()}
+        style={styles.button}
       >
-        <Text style={styles.buttonText}>Go Back</Text>
+        <Text>Search Cocktail</Text>
       </TouchableOpacity>
-    </View>
+      {data.map((item, index) => (
+        <View key={index} style={styles.feed}>
+          {item.strDrinkThumb && <Image style={styles.image} source={{ uri: item.strDrinkThumb }} />}
+          <View style={styles.text}>
+            <Text>Name: {item.strDrink}</Text>
+            <Text>Category: {item.strCategory}</Text>
+            {/* Add more properties you want to display */}
+          </View>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingTop: 10
+  },
+  image: {
+    height: 200,
+    width: 200,
+  },
+  feed: {
+    display: 'flex',
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  text: {
+    width: 380,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  button: {
+    width: 150,
+    backgroundColor: 'pink',
+    borderRadius: 5,
+    display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 25,
-  },
-  customButton: {
-    backgroundColor: '#fff3fd', 
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-  },
-  buttonText: {
-    color: 'black', 
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  textSize: {
-    fontSize: 18,
-    
+    alignSelf: 'center',
+    marginVertical: 10,
+    paddingVertical: 5,
   }
-});
+};
--- screens/Dinner.js
+++ screens/Dinner.js
@@ -1,46 +1,76 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'; 
+import React, { useState } from 'react';
+import { ScrollView, Text, View, TouchableOpacity, Image } from 'react-native';
+import axios from 'axios';
 
-export default function About({ navigation }) {
+export default function Home() {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('a');
+
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchTerm}`;
+
+  const grabMealInfo = () => {
+    axios.get(url)
+      .then((response) => {
+        console.clear();
+        setData(response.data.meals);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.textSize}>
-    Dinner recipes using API
-      </Text>
-      <StatusBar style="auto" />
+    <ScrollView style={styles.container}>
       <TouchableOpacity
-        style={styles.customButton}
-        onPress={() => navigation.goBack()}
+        onPress={() => grabMealInfo()}
+        style={styles.button}
       >
-        <Text style={styles.buttonText}>Go Back</Text>
+        <Text>Search Meals Starting with "A"</Text>
       </TouchableOpacity>
-    </View>
+      {data.map((item, index) => (
+        <View key={index} style={styles.feed}>
+          {item.strMealThumb && <Image style={styles.image} source={{ uri: item.strMealThumb }} />}
+          <View style={styles.text}>
+            <Text>Name: {item.strMeal}</Text>
+            <Text>Category: {item.strCategory}</Text>
+            {/* Add more properties you want to display */}
+          </View>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingTop: 10
+  },
+  image: {
+    height: 200,
+    width: 200,
+  },
+  feed: {
+    display: 'flex',
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  text: {
+    width: 380,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  button: {
+    width: 250,
+    backgroundColor: 'pink',
+    borderRadius: 5,
+    display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 25,
-  },
-  customButton: {
-    backgroundColor: '#fff3fd', 
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-  },
-  buttonText: {
-    color: 'black', 
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  textSize: {
-    fontSize: 18,
-    
+    alignSelf: 'center',
+    marginVertical: 10,
+    paddingVertical: 5,
   }
-  
-});
+};
